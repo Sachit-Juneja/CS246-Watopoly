100e027a0581c1378dd4ce69e3e4895bda21dc26
Arav.Sharma <a655shar@uwaterloo.ca>
2025-03-22 17:48:38 -0400

    updated buildings.h

--- buildings.h
+++ buildings.h
@@ -6,13 +6,16 @@
 using namespace std;
 // #include "players.h" -> commented out for now, will be added once Austin is done with Players.
 
+class Player; //forward declaration
+
 class Building{
-    // vector<Player> currentPlayers; -> uncomment once Player class is done. 
+    vector<Player *> currentPlayers;//-> uncomment once Player class is done. 
+
     public:
-        virtual void addPlayer() = 0;
-        virtual void removePlayer() = 0;
-        virtual void getCurrentPlayer() = 0;
-        virtual void event() = 0;
+        virtual void addPlayer(Player *p) = 0;
+        virtual void removePlayer(Player *p) = 0;
+        virtual vector<Player*> getCurrentPlayer() = 0;
+        virtual void event(Player *p) = 0; // handles the response of the square(building when p lands on it). 
 };
 
 #endif
