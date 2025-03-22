c69d47b044cb23a87ff6f703e90c85ed5747deae
a32wu <a32wu@uwaterloo.ca>
2025-03-22 17:46:29 -0400

    Player Class Done

---
+++ player.cc
@@ -0,0 +1,32 @@
+#include "player.h"
+#include <string>
+
+using namespace std;
+
+Player::Player(std::string name): name{name} {
+
+}
+        
+void Player::setPosition(int building) {
+    currentPosition = building;
+}
+
+int Player::getPosition() {
+    return currentPosition;
+}
+
+
+int Player::move(int numBuildings) {
+    currentPosition = (((currentPosition + numBuildings) % 40) + 40) % 40; 
+    return currentPosition;
+}
+
+string Player::getName() {
+    return name;
+}
+
+int Player::addMoney(int amount) {
+    money += amount;
+    return money; 
+}
+
---
+++ player.h
@@ -0,0 +1,30 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+class Player {
+
+    std::string name;
+    int currentPosition = 0;
+    int money = 1500;
+    
+    public:
+        Player(std::string name); // Constructor
+        
+        void setPosition(int building);
+        int getPosition(); 
+  
+        // Calculates and moves player directly.
+        // Modifies: currentPosition
+        // Returns: Index of the new position/building it is on. */
+        int move(int numBuildings); 
+
+        std::string getName();
+
+        // Adds or removes money from the player
+        // Returns: New amount of money in player after calculations.
+        int addMoney(int amount); 
+};
+
+#endif
