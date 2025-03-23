20e6a92c37c18772db45a47e86d30952d7184609
ss2junej <ss2junej@uwaterloo.ca>
2025-03-23 16:52:19 -0400

    Added methods to buildings common to property and non property
    Also fixed non prop. bldgs cc and header files

--- buildings.cc
+++ buildings.cc
@@ -1,4 +1,22 @@
 #include "buildings.h"
 
-Buildings::Buildings(string &name, int pos): building_name{name}, POS{pos} {}
+// Constructor: Initializes building name and position
+Buildings::Buildings(string &name, int pos) : building_name{name}, POS{pos} {}
 
+// Adds a player to the currentPlayers vector
+void Buildings::addPlayer(Player *p) {
+    currentPlayers.push_back(p);
+}
+
+// Removes a player from the currentPlayers vector
+void Buildings::removePlayer(Player *p) {
+    auto it = find(currentPlayers.begin(), currentPlayers.end(), p);
+    if (it != currentPlayers.end()) {
+        currentPlayers.erase(it);
+    }
+}
+
+// Returns the current list of players on the building
+vector<Player*> Buildings::getCurrentPlayer() {
+    return currentPlayers;
+}
--- nonpropertybuilding.h
+++ nonpropertybuilding.h
@@ -3,29 +3,14 @@
 
 #include "buildings.h"
 
-// Abstract class representing buildings that cannot be owned by players.
-// Examples: Goose Nesting, OSAP, DC Tims Line, etc.
+// Abstract class for non-ownable buildings (e.g., OSAP, Goose Nesting)
 class NonPropertyBuilding : public Buildings {
 public:
-    // Constructor: initializes name and board position of the building.
     NonPropertyBuilding(std::string &name, int pos);
 
-    // Adds a player to the current building.
-    // Used when a player lands on or moves into this building.
-    void addPlayer(Player *p) override;
-
-    // Removes a player from the current building.
-    // Used when a player moves out of this building.
-    void removePlayer(Player *p) override;
-
-    // Returns a list of players currently on this building.
-    std::vector<Player*> getCurrentPlayer() override;
-
-    // Abstract method to define the building-specific event that occurs
-    // when a player lands on this building.
+    // Only event is pure virtual now; others are inherited
     virtual void event(Player *p) override = 0;
 
-    // Virtual destructor.
     virtual ~NonPropertyBuilding() = default;
 };
 
