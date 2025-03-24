54d75b66115ef38888338b1b40c82c0cd1e5b53a
ss2junej <ss2junej@uwaterloo.ca>
2025-03-24 17:07:56 -0400

    call me the goat rahhhhhh

--- pb-gyms.h
+++ pb-gyms.h
@@ -7,7 +7,7 @@ class PBGyms : public PropertyBuildingsNew {
 public:
     PBGyms(std::string name, int pos, int cost, std::string Faculty);
 
-    void event(Player *p) override;
+    void event(Player *p, int rollTotal); // Now accepts dice total. Have to double check on how we can pass that through. somehting to do with board class
 
     ~PBGyms() override = default;
 };
