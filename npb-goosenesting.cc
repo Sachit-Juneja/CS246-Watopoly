83d975f9c9c4c01f8db2f987da19ee770e2116c8
ss2junej <ss2junej@uwaterloo.ca>
2025-03-23 23:50:54 -0400

    hardcoded position of nesting

--- npb-goosenesting.cc
+++ npb-goosenesting.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 
 NPBGooseNesting::NPBGooseNesting()
-    : NonPropertyBuilding("Goose Nesting", 1) {}
+    : NonPropertyBuilding("Goose Nesting", 20) {}
 
 void NPBGooseNesting::event(Player *p) {
     std::cout << p->getName() << " has been attacked by nesting geese at Goose Nesting... but nothing happens." << std::endl;
