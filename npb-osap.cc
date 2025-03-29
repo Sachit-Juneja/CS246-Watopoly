f5cd4ed895d6876d4ab3a989354b6ac0e3725309
a32wu <a32wu@uwaterloo.ca>
2025-03-29 16:06:44 -0400

    Fixed Osap

--- npb-osap.cc
+++ npb-osap.cc
@@ -3,6 +3,5 @@
 Osap::Osap(): NonPropertyBuilding{"OSAP", 0} {}
 
 void Osap::event(Player * p) {
-    p->addMoney(200);
-    cout << p->getName() << " has passed OSAP and has collected $200!" << endl;
+
 }
